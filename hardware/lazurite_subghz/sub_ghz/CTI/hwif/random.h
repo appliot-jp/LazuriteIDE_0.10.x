220548dfa2397f5ff127c58264fd46f4b1e7b167
Naotaka Saito <naotaroooo22@gmail.com>
2017-01-25 18:11:48 +0900

    test for subtree

--- hardware/lazurite_subghz/sub_ghz/CTI/hwif/random.h
+++
@@ -1,8 +0,0 @@
-#ifndef LAZURITE_IDE
-#ifndef _RANDOM_H_
-#define _RANDOM_H_
-#endif	// LAZURITE_IDE
-#include <linux/kernel.h>
-extern uint16_t rand(void);
-extern void srand(uint16_t data);
-#endif
