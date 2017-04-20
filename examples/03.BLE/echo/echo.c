b3d6335199f546f4dec1c170cb169c25d78a2978
Naotaka Saito <naotaroooo22@gmail.com>
2017-04-20 11:18:04 +0900

    add examples of tx64 & rx.

--- examples/03.BLE/echo/echo.c
+++ examples/03.BLE/echo/echo.c
@@ -5,12 +5,11 @@
 
 void setup() {
   // put your setup code here, to run once:
-
-
 	pinMode(BLUE_LED,OUTPUT);
 	pinMode(ORANGE_LED,OUTPUT);
 
 	ble.begin();
+	Serial3.begin(115200);
 //	ble.advertising(true);
 	
 }
@@ -22,8 +21,11 @@ void loop() {
 	if(ble.getStatus() == 1) {
 		ble.advertising(true);
 	}
-	if(ble.available())
+	if(ble.available()>0)
 	{
-		ble.write_byte(ble.read());
+		Serial3.write_byte(ble.read());
+	}
+	if(Serial3.available()>0) {
+		ble.write_byte(Serial1.read());
 	}
 }
