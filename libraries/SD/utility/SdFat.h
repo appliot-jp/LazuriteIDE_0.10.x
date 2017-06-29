da739a3e7aca136786c27c4f3b5de61ee6cf3d81
Yoshiyuki Tanaka <tanaka802@dns.lapis-semi.com>
2017-06-29 15:45:53 +0900

    modified license term of SD card lib

--- libraries/SD/utility/SdFat.h
+++ libraries/SD/utility/SdFat.h
@@ -1,22 +1,23 @@
-/* Arduino SdFat Library
- * Copyright (C) 2009 by William Greiman
+/* FILE NAME: FatStructs.h
  *
- * This file is part of the Arduino SdFat Library
+ * Copyright (c) 2017  Lapis Semiconductor Co.,Ltd.
+ * All rights reserved.
  *
- * This Library is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * This Library is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
  *
  * This Library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with the Arduino SdFat Library.  If not, see
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/>.
  */
+
 #ifndef SdFat_h
 #define SdFat_h
 
