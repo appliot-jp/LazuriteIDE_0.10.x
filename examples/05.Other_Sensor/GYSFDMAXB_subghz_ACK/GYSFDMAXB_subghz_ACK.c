19d8edb7c69f47b56c5844d8522ddb9110fd04d4
Yoshiyuki Tanaka <tanaka802@dns.lapis-semi.com>
2017-08-02 18:50:22 +0900

    update GYSFDMAXB_subghz_ACK sample

--- examples/05.Other_Sensor/GYSFDMAXB_subghz_ACK/GYSFDMAXB_subghz_ACK.c
+++ examples/05.Other_Sensor/GYSFDMAXB_subghz_ACK/GYSFDMAXB_subghz_ACK.c
@@ -23,80 +23,6 @@
  * THE SOFTWARE.
  */
 
-/*
- [Premise]
-
- GPS module(GYSFDMAXB) sends the data every 1 second.
- GPS baud rate = 9600bps ( 1/(9600 bps/10bit) * 1000 = 1.04ms/byte)
- SubGHz transfer rate = 100kbps ( 1/(100000 bps/8bit) * 1000 = 0.08ms/byte)
- Serial FIFO(driver) size = 128 byte
-
- [Design policy]
-
- GPS data lost is OK when there is no ACK after 1 retry of SubGHz send.
-
- Buffer size = 220 byte
- Retry = 1 time
- ACK timeout = 50 ms
- 
- [Normal case]
-
-                |<--------------------- 1s --------------------->|
- GPS receive    ****************************                     **************
- Buffer         ~~~~~~~~~~~~ ~~~~~~~~~~~~ ~~                     ~~~~~~~~~~ ~~~
- SubGHz send                +            +                                 +
- receive ACK                -            -                                 -
- Serial FIFO                =            =                                 =
-
-  * : gets data from GPS
-  ~ : copies data from Serial FIFO until Buffer is full
-     (It spends time more than 229ms=220byte * 1.04ms/byte at least)
-  + : sends data via SubGHz
-     (It spends time more than 17.6ms=220byte * 0.08ms/byte at least)
-  - : receive ACK immediately (3ms)
-  = : increases Serial FIFO data
-     (It increase more than 17byte=17.6ms / 1.04ms/byte at least)
-
- [Send NG but Retry is OK]
-
-                |<--------------------- 1s --------------------->|
- GPS receive    ****************************                     **************
- Buffer         ~~~~~~~~~~~~     ~~~~~~~ ~~~                     ~~~~~~~~~ ~~~~
- SubGHz send                +  +        +                                 +
- ACK wait                   --- -       -                                 -
- Serial FIFO                =====       =                                 =
-
-  * : gets data from GPS
-  ~ : copies data from Serial FIFO until Buffer is full
-     (It spends time more than 229ms=220byte * 1.04ms/byte at least)
-  + : sends data via SubGHz
-     (It spends time more than 17.6ms=220byte * 0.08ms/byte at least)
-  - : receive ACK after retry
-     (It spends time more than 70.6ms=50ms timeout+17.6ms send time+3ms ACK at least)
-  = : increases Serial FIFO data
-     (It increase more than 68byte=70.6ms / 1.04ms/byte at least)
-
- [Send NG and Retry is NG(Worst case)]
-
-                |<--------------------- 1s --------------------->|
- GPS receive    ****************************                     **************
- Buffer         ~~~~~~~~~~~~       ~~~~~ ~~~                     ~~~~~~~~~ ~~~~
- SubGHz send                +  +        +                                 +
- ACK wait                   --- ---     -                                 -
- Serial FIFO                ======X     =                                 =
-
-  * : gets data from GPS
-  ~ : copies data from Serial FIFO until Buffer is full
-     (It spends time more than 229ms=220byte * 1.04ms/byte at least)
-  + : sends data via SubGHz
-     (It spends time more than 17.6ms=220byte * 0.08ms/byte at least)
-  - : receive no ACK after retry
-     (It spends time more than 135.2ms=50ms x2 timeout+17.6ms x2 send time at least)
-  = : increases Serial FIFO data, but overflowed!(X)
-     (It increase more than 130byte=135.2ms / 1.04ms/byte at least)
-
-*/
-
 #include "GYSFDMAXB_subghz_ACK_ide.h"		// Additional Header
 
 #define LED					( 26 )			// pin number of Blue LED
@@ -104,18 +30,31 @@
 #define SUBGHZ_PANID		( 0xabcd )		// panid
 #define HOST_ADDRESS		( 0x5f59 )		// distination address
 #define GPS_BUF_SIZE		( 220 )			// more than 128 byte at least
-#define SUBGHZ_RETRY_NUM	( 1 )			// number of retry
-#define SUBGHZ_ACK_TIMEOUT	( 50 )			// ACK timeout
+#define GPS_SEN_SIZE		( 6 )
+#define NMEA_GPRMC			( "$GPRMC" )
 
 uint8_t bufp;
 uint8_t gps_buf[GPS_BUF_SIZE];
 
-void gps_output()
+void gps_output(void)
 {
-	digitalWrite(LED,LOW);								// LED ON
-	Serial.println("send");
-	SubGHz.send(SUBGHZ_PANID, HOST_ADDRESS, gps_buf, GPS_BUF_SIZE, NULL);	// send data
-	digitalWrite(LED,HIGH);								// LED off
+	SUBGHZ_MSG msg;
+	uint32_t tmp;
+
+	if (strncmp(gps_buf, NMEA_GPRMC, GPS_SEN_SIZE) == 0) {
+		digitalWrite(LED,LOW);								// LED ON
+		Serial.println("send");
+		tmp = millis();
+		msg = SubGHz.send(SUBGHZ_PANID, HOST_ADDRESS, gps_buf, bufp, NULL);	// send data
+		digitalWrite(LED,HIGH);								// LED off
+		if (msg == SUBGHZ_TX_ACK_FAIL) {
+			Serial.print("Couldn't receive ACK even after ");
+			Serial.print_long(millis()-tmp,DEC);
+			Serial.println(" ms passed.");
+			Serial2.flush();
+		}
+	}
+
 }
 
 void setup() {
@@ -126,10 +65,6 @@ void setup() {
 	digitalWrite(LED,HIGH);								// setting of LED
 
 	SubGHz.init();										// initializing Sub-GHz
-	SubGHz.getSendMode( & param);
-	param.txRetry = SUBGHZ_RETRY_NUM;					// change number of retry
-	param.txInterval = SUBGHZ_ACK_TIMEOUT;				// change ACK timeout
-	SubGHz.setSendMode( & param);
 	SubGHz.begin(SUBGHZ_CH, SUBGHZ_PANID, SUBGHZ_100KBPS, SUBGHZ_PWR_20MW);		// start Sub-GHz
 
 	Serial.begin(115200);
@@ -139,11 +74,18 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+	uint8_t data;
+
 	if (Serial2.available() > 0) {
-		gps_buf[bufp++] = (uint8_t) Serial2.read();
-		if (bufp >= GPS_BUF_SIZE) {
-			gps_output();
+		data = (uint8_t) Serial2.read();
+		if ((data == 0x0D) || (data == 0x0A)) {			// if end of line
+			if ((bufp > 0) && (bufp < GPS_BUF_SIZE)) {
+				gps_buf[bufp] = NULL;
+				gps_output();
+			}
 			bufp = 0;
+		} else if (bufp < GPS_BUF_SIZE) {
+			gps_buf[bufp++] = data;
 		}
 	}
 }
