64c3a65497b3d4dff7c12e77c5686fde75b12b64
NaotakaSaito <naotaroooo22@gmail.com>
2020-02-05 13:08:19 +0900

    update rxtx_subghz in sample. add function. update lazurite3_mk74040.bld for new LazDriver.

--- examples/03.SubGHz/RxTx_SubGHz/RxTx_SubGHz.c
+++ examples/03.SubGHz/RxTx_SubGHz/RxTx_SubGHz.c
@@ -2,20 +2,20 @@
 
 /* FILE NAME: RxTx_SubGHz.c
  * The MIT License (MIT)
- * 
+ *
  * Copyright (c) 2015  Lapis Semiconductor Co.,Ltd.
  * All rights reserved.
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -25,11 +25,16 @@
  * THE SOFTWARE.
 */
 
-#define SUBGHZ_CH	36
+static uint8_t ch = 36;
+static uint8_t baud = 100;
+#define DEFAULT_PAYLOAD		__FILE__
 #define SUBGHZ_PANID	0xABCD
-#define SUBGHZ_DST      0x7FAE
-#define SUBGHZ_PAYLOAD	"TX_RX_SAMPLE"
-#define RX_WINDOW		100
+#define SUBGHZ_DST      0x1234
+static char serialBuf[256];
+static int serialBufIndex = 0;
+static uint8_t payload[256];
+static char txmode = 0;
+static uint32_t txinterval=100;
 uint8_t rx_data[256];
 
 SUBGHZ_STATUS rx;							// structure for getting rx status
@@ -42,136 +47,206 @@ static unsigned long last_tx_time;
 
 void print_hex_func(uint8_t data)
 {
-	if(data == 0) Serial.print("00");
-	else if(data < 16) {
-		Serial.print("0");
-		Serial.print_long(data,HEX);
-	} else {
-		Serial.print_long(data,HEX);
-	}
+  if (data == 0) {
+    Serial.print("00");
+  }
+  else
+    if (data < 16) {
+      Serial.print("0");
+      Serial.print_long(data, HEX);
+    } else {
+      Serial.print_long(data, HEX);
+    }
 }
 
 
 void setup(void)
 {
-	SUBGHZ_MSG msg;
-	uint8_t myAddr[8];
+  SUBGHZ_MSG msg;
+  uint8_t myAddr[8];
 
-	Serial.begin(115200);
-	
-	msg = SubGHz.init();
-	if(msg != SUBGHZ_OK)
-	{
-		SubGHz.msgOut(msg);
-		while(1){ }
-	}
-	
-	SubGHz.getMyAddr64(myAddr);
-	Serial.print("myAddress = ");
-	print_hex_func(myAddr[0]);
-	print_hex_func(myAddr[1]);
-	Serial.print(" ");
-	print_hex_func(myAddr[2]);
-	print_hex_func(myAddr[3]);
-	Serial.print(" ");
-	Serial.print(" ");
-	print_hex_func(myAddr[4]);
-	print_hex_func(myAddr[5]);
-	Serial.print(" ");
-	print_hex_func(myAddr[6]);
-	print_hex_func(myAddr[7]);
-	Serial.println("");
+  Serial.begin(115200);
+
+  msg = SubGHz.init();
+  if (msg != SUBGHZ_OK)
+  {
+    SubGHz.msgOut(msg);
+    while (1) { }
+  }
+  memcpy(payload, DEFAULT_PAYLOAD, sizeof(DEFAULT_PAYLOAD));
+
+  SubGHz.getMyAddr64(myAddr);
+  Serial.print("myAddress = ");
+  print_hex_func(myAddr[0]);
+  print_hex_func(myAddr[1]);
+  Serial.print(" ");
+  print_hex_func(myAddr[2]);
+  print_hex_func(myAddr[3]);
+  Serial.print(" ");
+  Serial.print(" ");
+  print_hex_func(myAddr[4]);
+  print_hex_func(myAddr[5]);
+  Serial.print(" ");
+  print_hex_func(myAddr[6]);
+  print_hex_func(myAddr[7]);
+  Serial.println("");
 
 
-	SubGHz.setKey(aes_key);
-	
-	pinMode(BLUE_LED,OUTPUT);
-	digitalWrite(BLUE_LED,HIGH);
-	
-	Serial.println("TIME	HEADER	SEQ	PANID	RX_ADDR	TX_ADDR	RSSI	PAYLOAD");
-	Serial.println("-----------------------------------------------------------------------------------------------------------------");
+  SubGHz.setKey(aes_key);
 
-	// リセット後にデータを送信させるためのダミーデータ
-	last_tx_time = 0x8000;
-	return;
+  digitalWrite(BLUE_LED, HIGH);
+  digitalWrite(ORANGE_LED, HIGH);
+  pinMode(BLUE_LED, OUTPUT);
+  pinMode(ORANGE_LED, OUTPUT);
+
+  Serial.println("TIME	HEADER	SEQ	PANID	RX_ADDR	TX_ADDR	RSSI	PAYLOAD");
+  Serial.println("-----------------------------------------------------------------------------------------------------------------");
+
+  // リセット後にデータを送信させるためのダミーデータ
+  last_tx_time = 0x8000;
+
+  SubGHz.begin(ch, SUBGHZ_PANID,  baud, SUBGHZ_PWR_20MW);
+  SubGHz.rxEnable(NULL);
+
+  return;
 }
 
 void loop(void)
 {
-	SUBGHZ_MAC_PARAM mac;
-	short rx_len;
-	short index=0;
-	uint16_t data16;
-	uint32_t tx_time;
-	uint32_t current_time = millis();
+  SUBGHZ_MSG msg;
+  SUBGHZ_MAC_PARAM mac;
+  short rx_len;
+  short index = 0;
+  uint16_t data16;
+  int rtmp = 0;
 
-	// 送信処理
-	SubGHz.begin(SUBGHZ_CH, SUBGHZ_PANID,  SUBGHZ_100KBPS, SUBGHZ_PWR_20MW);
-	SubGHz.rxEnable(NULL);
-	digitalWrite(BLUE_LED,LOW);
-	SubGHz.send(SUBGHZ_PANID,SUBGHZ_DST,SUBGHZ_PAYLOAD,sizeof(SUBGHZ_PAYLOAD),NULL);
-	tx_time = current_time;
-	digitalWrite(BLUE_LED,HIGH);
+  if(txmode == 0) {
+	  if ((millis() - last_tx_time) > txinterval) {
+	    digitalWrite(BLUE_LED, LOW);
+	    msg = SubGHz.send(SUBGHZ_PANID, SUBGHZ_DST, payload, strlen(payload), NULL);
+	    SubGHz.msgOut(msg);
+	    last_tx_time = millis();
+	    digitalWrite(BLUE_LED, HIGH);
+	  }
+  }
+  //受信時の処理
+  rx_len = SubGHz.readData(rx_data, sizeof(rx_data));
+  if (rx_len > 0)
+  {
+    //受信データの最後にNULLを付与する
+    rx_data[rx_len] = 0;
+    digitalWrite(ORANGE_LED, LOW);
+    SubGHz.getStatus(NULL, &rx);
+    SubGHz.decMac(&mac, rx_data, rx_len);
+    Serial.print_long(millis(), DEC);
+    Serial.print("\t");
 
-	// 送信後にRX_WINDOW(ms)の時間だけ受信用の窓を開く
-	while((current_time - tx_time) < RX_WINDOW ){
-		current_time = millis();
-		rx_len = SubGHz.readData(rx_data,sizeof(rx_data));
-		//受信データの最後にNULLを付与する
-		rx_data[rx_len]=0;
-		//受信時の処理
-		if(rx_len>0)
-		{
-			digitalWrite(ORANGE_LED, LOW);
-			SubGHz.getStatus(NULL,&rx);	
-			SubGHz.decMac(&mac,rx_data,rx_len);
-			Serial.print_long(millis(),DEC);
-			Serial.print("\t");
-	
-			Serial.print_long(mac.mac_header.fc16,HEX);
-			Serial.print("\t");
-	
-			Serial.print_long(mac.seq_num,HEX);
-			Serial.print("\t");
-	
-			Serial.print_long(mac.dst_panid,HEX);
-			Serial.print("\t");
-	
-			print_hex_func(mac.dst_addr[7]);
-			print_hex_func(mac.dst_addr[6]);
-			print_hex_func(mac.dst_addr[5]);
-			print_hex_func(mac.dst_addr[4]);
-			print_hex_func(mac.dst_addr[3]);
-			print_hex_func(mac.dst_addr[2]);
-			print_hex_func(mac.dst_addr[1]);
-			print_hex_func(mac.dst_addr[0]);
-			Serial.print("\t");
-	
-			print_hex_func(mac.src_addr[7]);
-			print_hex_func(mac.src_addr[6]);
-			print_hex_func(mac.src_addr[5]);
-			print_hex_func(mac.src_addr[4]);
-			print_hex_func(mac.src_addr[3]);
-			print_hex_func(mac.src_addr[2]);
-			print_hex_func(mac.src_addr[1]);
-			print_hex_func(mac.src_addr[0]);
-			Serial.print("\t");
-	
-			Serial.print_long(rx.rssi,DEC);
-			Serial.print("\t");
-			
-			Serial.print(mac.payload);
-			// print ln
-			Serial.println("");
-			digitalWrite(ORANGE_LED, HIGH);
-			break;
-		}
-	}
-	// 処理完了後にスリープする
-	SubGHz.rxDisable();
-	SubGHz.close();
-	sleep(10000L);
-	
-	return;
+    Serial.print_long(mac.mac_header.fc16, HEX);
+    Serial.print("\t");
+
+    Serial.print_long(mac.seq_num, HEX);
+    Serial.print("\t");
+
+    Serial.print_long(mac.dst_panid, HEX);
+    Serial.print("\t");
+
+    print_hex_func(mac.dst_addr[7]);
+    print_hex_func(mac.dst_addr[6]);
+    print_hex_func(mac.dst_addr[5]);
+    print_hex_func(mac.dst_addr[4]);
+    print_hex_func(mac.dst_addr[3]);
+    print_hex_func(mac.dst_addr[2]);
+    print_hex_func(mac.dst_addr[1]);
+    print_hex_func(mac.dst_addr[0]);
+    Serial.print("\t");
+
+    print_hex_func(mac.src_addr[7]);
+    print_hex_func(mac.src_addr[6]);
+    print_hex_func(mac.src_addr[5]);
+    print_hex_func(mac.src_addr[4]);
+    print_hex_func(mac.src_addr[3]);
+    print_hex_func(mac.src_addr[2]);
+    print_hex_func(mac.src_addr[1]);
+    print_hex_func(mac.src_addr[0]);
+    Serial.print("\t");
+
+    Serial.print_long(rx.rssi, DEC);
+    Serial.print("\t");
+
+    Serial.print(mac.payload);
+    // print ln
+    Serial.println("");
+    digitalWrite(ORANGE_LED, HIGH);
+  }
+  rtmp = Serial.read();
+  if (rtmp >= 0) {
+    if (serialBufIndex < (sizeof(serialBuf) - 2)) {
+      serialBuf[serialBufIndex] = (uint8_t)rtmp;
+      serialBufIndex++;
+    }
+  }
+  if ((rtmp == 10) || (rtmp == 13)) {
+    bool update = false;
+    serialBuf[serialBufIndex-1] = NULL;
+    rtmp = atoi(serialBuf);
+    if ((rtmp >= 24) && (rtmp <= 62)) {
+      ch = (uint8_t)rtmp;
+      update = true;
+    } else if (rtmp == 1) {
+      baud = 50;
+      update = true;
+    } else if (rtmp == 2) {
+      baud = 100;
+      update = true;
+    } else if (rtmp == 3) {
+      Serial.println("one shot");
+      txmode = 1;
+    } else if (rtmp == 4) {
+      Serial.println("10ms");
+      txinterval = 10;
+      txmode = 0;
+    } else if (rtmp == 5) {
+      Serial.println("100ms");
+      txinterval = 100;
+      txmode = 0;
+    } else if (rtmp == 6) {
+      txinterval = 1000;
+      txmode = 0;
+    } else if (rtmp == 7) {
+      txinterval = 10000;
+      txmode = 0;
+    } else {
+     	memcpy(payload,serialBuf,serialBufIndex);
+	  if (txmode == 1) {
+	    digitalWrite(BLUE_LED, LOW);
+	    msg = SubGHz.send(SUBGHZ_PANID, SUBGHZ_DST, payload, strlen(payload), NULL);
+	    SubGHz.msgOut(msg);
+	    last_tx_time = millis();
+	    digitalWrite(BLUE_LED, HIGH);
+	  }
+    }
+    serialBufIndex = 0;
+    
+    if (update) {
+      Serial.print("change RF params  CH=");
+      Serial.print_long((long)ch, DEC);
+      Serial.print(", baud=");
+      Serial.print_long((long)baud, DEC);
+      Serial.println("");
+      SubGHz.rxDisable();
+      msg = SubGHz.begin(ch, SUBGHZ_PANID,  baud, SUBGHZ_PWR_20MW);
+      if (msg != SUBGHZ_OK)
+      {
+        SubGHz.msgOut(msg);
+      }
+      msg = SubGHz.rxEnable(NULL);
+      if (msg != SUBGHZ_OK)
+      {
+        SubGHz.msgOut(msg);
+      }
+    }
+  }
+
+  return;
 }
 
