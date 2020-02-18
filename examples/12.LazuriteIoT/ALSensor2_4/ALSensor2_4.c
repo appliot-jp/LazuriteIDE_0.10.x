7885d347ad84ebb2f914d728b9822deac8493c02
YoshiyukiTanaka <tanaka802@dsn.lapis-semi.com>
2020-02-18 14:43:30 +0900

    update ALSensor, ALSensor2 sample for Lazurite IoT

--- examples/12.LazuriteIoT/ALSensor2_4/ALSensor2_4.c
+++ examples/12.LazuriteIoT/ALSensor2_4/ALSensor2_4.c
@@ -1,6 +1,6 @@
-#include "ALSensor_3_ide.h"		// Additional Header
+#include "ALSensor2_4_ide.h"		// Additional Header
 
-/* FILE NAME: ALSensor_3.c
+/* FILE NAME: ALSensor2_4.c
  * The MIT License (MIT)
  *
  * Copyright (c) 2018  Lapis Semiconductor Co.,Ltd.
@@ -30,10 +30,20 @@
  * this function is called in initalizing process
  * return filename
  */
+
+static int data_buf_index = 0;
+#define DATA_BUF_LENGTH  8
+static float data_buf[DATA_BUF_LENGTH];
+
+static void mstimer2_isr(void) {
+	waitEventFlag = true;
+	return;
+}
+
 char* sensor_init() {
 	static char filename[] = __FILE__;
+	useInterruptFlag = true;
 	Wire.begin();
-	rpr0521rs.init();
 	return filename;
 }
 
@@ -43,24 +53,30 @@ char* sensor_init() {
  *         false: sensor_meas is called immidialtely
  */
 bool sensor_activate(void) {
+	uint8_t reg = 0x86;
+	for(data_buf_index=0;data_buf_index < DATA_BUF_LENGTH; data_buf_index++) {
+		data_buf[data_buf_index] = 0;
+	}
+	data_buf_index=0;
+	timer2.set(100L,mstimer2_isr);
+	timer2.start();
+	rpr0521rs.write(RPR0521RS_MODE_CONTROL, &reg, sizeof(reg));
+	Serial.begin(115200);
 	return false;
 }
 /*
  * callback function of deactivation
  */
 void sensor_deactivate(void) {
-	return;
+	timer2.stop();
 }
 
 /*
  * function of sensor measurement
- *
- * s[]: Array of SensorState is passed. If single sensor type, array size is always '1'.
- *
  * val->data is settled depends on data type
  * data type is set into val->type
  * val->digit shows digit of floating number.
- * 
+ *
  * val->data.uint8_val=xxx;   val->type = UINT8_VAL;
  * val->data.int8_val=xxx;    val->type = INT8_VAL;
  * val->data.uint16_val=xxx;  val->type = UINT16_VAL;
@@ -73,24 +89,31 @@ void sensor_deactivate(void) {
 void sensor_meas(SensorState s[]) {
 	SENSOR_VAL *val = &(s[0].sensor_val);
 	float als_val;
-	uint8_t reg = 0x86, data[6];
+	int i;
+	uint8_t rawval[4];
 	uint16_t rawals[2];
 
-//	rpr0521rs.get_oneShot(&ps_val, &als_val);
+	rpr0521rs.read(RPR0521RS_ALS_DATA0_LSB, rawval, 4);
 
-	rpr0521rs.write(RPR0521RS_MODE_CONTROL, &reg, sizeof(reg));
-	sleep(100);			// measurement time
-
-	rpr0521rs.get_rawpsalsval(data);
-	
-	rawals[0] = ((unsigned short)data[3] << 8) | data[2];
-	rawals[1] = ((unsigned short)data[5] << 8) | data[4];
+	rawals[0] = ((unsigned short)rawval[1] << 8) | rawval[0];
+	rawals[1] = ((unsigned short)rawval[3] << 8) | rawval[2];
 
 	als_val = rpr0521rs.convert_lux(rawals);
+	data_buf[data_buf_index] = als_val;
+	data_buf_index++;
 
-	reg = 0;
-	rpr0521rs.write(RPR0521RS_MODE_CONTROL, &reg, sizeof(reg));
+	if(data_buf_index >= 8) data_buf_index = 0;
+
+	als_val = 0;
+	for(i=0;i<DATA_BUF_LENGTH;i++) {
+		als_val += data_buf[i];
+	}
+	als_val = als_val/DATA_BUF_LENGTH;
 	val->data.double_val=als_val;  val->type = DOUBLE_VAL; val->digit = 2;
-	
+
+	Serial.print("STX,");
+	Serial.print_double((double)als_val,2);
+	Serial.println(",ETX");
+
 	return;
 }
